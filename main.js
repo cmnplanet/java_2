dab1a4c5c46c384abbd912fd4f990ebed283397a
cmnplanet <cmnfv@mac.com>
2017-07-28 09:54:37 -0400

    third try

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
 // 1.
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // Then, write and example of using the function.
-function max(num1, num2){
-  if ((num1 > num2) || (num1 < num2));{
-    return max;
-  } else {
-    return ""
+
+function max (x, y) {
+  if (x < y) {
+    return y;
+  } else { (x > y)
+    return x;
   }
 }
 
-console.log(max(2,1));
-console.log(max(1,2));
+console.log(max(2, 3));
+console.log(max(2, 1));
+
+    // Your answer here
 
 // 2.
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // Then, write and example of using the function.
 
-function max(num1, num2, numb){
-  if num1 > num2;
-  return max;
-} else {
-  if num2 > num1;
-  return max;}
-}
+function maxOfThree(x, y, z){
+  if ((x > y) || (x > z)) {
+    return x;
+  } else if ((z > y) || (z > x)) {
+    return z;
+  } else if ((y > z) || (y > x)) {
+    return y;
+    }
+  }
 
-console.log(max(10, 2));
-console.log(max(2, 10));
+  console.log(maxOfThree(2, 3, 4));
+  console.log(maxOfThree(4, 1, 2));
+  console.log(maxOfThree(1, 4, 1));
 
 
 
@@ -34,55 +40,101 @@ console.log(max(2, 10));
 // Then, write and example of using the function.
 
 function isVowel(char){
-  if char(a || e || i || o || u)
-  return 'True!'
-  else char(!== a || e || i || o || u)
-  return 'False!'
-    // Your answer here
+  if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+    return true;
+  } else { (char !== "a" || char !== "e" || char !== "i" || char !== "o" || char !== "u")
+    return false;
+  }
 }
+
+console.log(isVowel("a"));
+console.log(isVowel("q"));
 
 
 // 4.
 // Write a function called `sum` that takes two parameters and returns the sum of those 2 numbers.
 // Then, write and example of using the function.
+//
+//
 
-function sum(numbA, numbB){
-  return numbA + numbB
+function sum(x, y){
+  return x + y
 }
 
-console.log(sum(1,3))
+console.log(sum(3,4))
+console.log(sum(12,100))
 
-
-
+//
 // 5.
 // Write a function named `avg` that takes 3 parameters and returns the average of those 3 numbers.
 // Then, write and example of using the function.
+//
+//
+
+function avg(i, k, n){
+  return (i + k + n) / 3
+}
+
+console.log(avg(12,7,100))
 
 
-
+//
 // 6.
 // Write a function called `getLength` that takes one parameter (a string) and returns the length
 // Then, write and example of using the function.
 
+function getLength(x){
+  return "12 Feet"
+}
+
+console.log(getLength("Shoe"))
 
 
+//
+//
+//
 // 7.
 // Write a function called `greaterThan` that takes two parameters
 // and returns `true` if the second parameter is greater than the first.
 // Otherwise the function should return `false`.
 // Then, write and example of using the function.
 
+function greaterThan(r,g){
+  if (r < g) {
+  return true;
+} else {
+  (g > r)
+  return false;
+}
+
+}
+
+console.log(greaterThan(6, 12))
+console.log(greaterThan(12, 6))
 
 
+
+//
+//
+//
 // 8.
 // Write a function called `greet` that takes a
 // single parameter and returns a string that
 // is formated like "Hello, Name!" where *Name*
 // is the parameter that was passed in.
 // Then, write and example of using the function.
+//
+//
+
+function greet(x){
+  return "Hello my name is " + x + "\!";
+}
+
+console.log(greet("Bobo"))
 
 
 
+//
 // 9.
 // Write a function called `madlib` that takes 4 or more parameters (words).
 // The function should insert the words into a pre-defined sentence.
@@ -91,3 +143,9 @@ console.log(sum(1,3))
 // words: "quick", "fox", "fence"
 // sentence: "quick brown fox jumps over the fence"
 // Then, write and example of using the function.
+
+function madlib(x, q, w, r){
+  return "This " + x + " is " + q + " but, " + w + " " + r + "\.";
+}
+
+console.log(madlib("tomato","purple","tastes", "delicious"))
